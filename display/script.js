8b77d75b6f6c51415c09fe24ff5f72d86fe78094
Ficho Pranandasya Andrian Pratama <93019687+FranPratama@users.noreply.github.com>
2023-12-13 09:30:53 +0700

    Add files via upload

--- display/script.js
+++ display/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
 function fetchItems(){
     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzAyNDMwNTgwLCJpYXQiOjE3MDI0MzAyODAsImp0aSI6Ijg1MzViMmQzNGE3NzQ2Nzg4MzAyNmNhMDhlMjE5ZDM3IiwidXNlcl9pZCI6MX0.9IhG8wX3BfDdhCOZd5oOu12a367YFlf2Dul0JwITOPM'
     // const token = localStorage.getItem('accessToken');
-    // fetch('http://127.0.0.1:8000/apia/item/')
+    // fetch('http://127.0.0.1:8000/apia/item/')    
     fetch('http://127.0.0.1:8000/apia/item/',{
         Headers : {
             'Authorization' : `Bearer ${token}`
