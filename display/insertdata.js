940141dfb4ed15a01199ea5d0097e0cdb94aa3e5
FranPratama <rkyouya0@gmail.com>
2024-01-03 08:21:35 +0700

    Initial commit

--- display/insertdata.js
+++ display/insertdata.js
@@ -1,8 +1,8 @@
 document.getElementById('addItemForm').addEventListener('submit', function(event){
     event.preventDefault();
 
-    const name = document.getElementById('nameItem').value;
-    const description = document.getElementById('descriptionItem').value;
+    const nameItem = document.getElementById('nameItem').value;
+    const descriptionItem = document.getElementById('descriptionItem').value;
     const token = localStorage.getItem('accessToken');
     
     fetch('http://127.0.0.1:8000/api/token/', {
@@ -13,11 +13,11 @@ document.getElementById('addItemForm').addEventListener('submit', function(event
         },
         body: JSON.stringify({
             name : nameItem,
-            description  : descriptionItem,
+            description  : descriptionItem
         })
     })
     .then(response => {
-        if(!response.ok){
+        if(response.ok){
             return response.json();
         }else{
             throw new Error('Something went wrong');
@@ -25,8 +25,9 @@ document.getElementById('addItemForm').addEventListener('submit', function(event
     })
     .then(data => {
         console.log('Success: ', data);
-        $('#addItemModal').modal('hide');
-        fetchItems();
+        $('#myModal').modal('hide');
+        // fetchItems();
+        window.location.reload();
     })
     .catch(error => {
         console.error('Error:', error);
--- display/script.js
+++ display/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function fetchItems(){
-    // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzAzMDM1MTg4LCJpYXQiOjE3MDMwMzQ4ODgsImp0aSI6IjcxYzEzZDg4YTllMzQxM2ViNjc2YmJkNDYzZmJjODg0IiwidXNlcl9pZCI6Mn0.XUHkATH4QA05GrpCIot55b6NZyDFlbooZK5DFtYA2mU'
+    // const token = ''
     const token = localStorage.getItem('accessToken');
     fetch('http://127.0.0.1:8000/apia/item/',{
         Headers : {
@@ -22,11 +22,11 @@ function displayitems(items){
         itemElement.classList.add('col-md-6');
         itemElement.innerHTML = `
         <div class="card mb-4">
-        <div class="card-body">
-        <h5 class="card-title"> ${item.name} </h5>
-        <p class="card-text"> ${item.description} </p>
+            <div class="card-body">
+                <h5 class="card-title"> ${item.name} </h5>
+                <p class="card-text"> ${item.description} </p>
+            </div>
         </div>
-        <div>
         `;
         itemsContainer.appendChild(itemElement);
     })
